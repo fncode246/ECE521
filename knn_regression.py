cdda4c9b8189e11b0e93531b4f5efe11cff0564b
Krist Papadopoulos <kristpapas@gmail.com>
2018-01-15 13:50:42 -0500

    updated knn_regression master file
    included code from Logans ipython notebook to execute knn regression with tensorflow
    
    need to check responsibility matrix execution and have a question of prediction

--- knn_regression.py
+++ knn_regression.py
@@ -5,15 +5,17 @@ ECE521 Assignment 1 - KNN Implementation for Classification and Regression
 
 KNN Regression Questions 1 and 2
 
-Created on Fri Jan 12 11:37:10 2018
+Edited on Mon Jan 15, 2018
 
-@author: KP
+@author: Krist Papadopoulos, Logan Rock, Yu Liu
 """
 
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-def D_euc(X, Z):
+
+def dist_euclid(X, Z):
     
     '''
     Compute squared Euclidean L2 distance between tensor X and tensor Z
@@ -25,65 +27,49 @@ def D_euc(X, Z):
     
     Z = tf.constant([[2, 4, 5], [1, 1, 1], [6,1,8]])
     
-    D_euc(X,Z) = [[ 1 29 27]
+    dist_euclid(X,Z) = [[ 1 29 27]
                 [34 16 50]]
-    
     '''
-    X_norm = tf.reshape(tf.reduce_sum(X**2,axis=1), [-1,1])
-    Z_norm = tf.reshape(tf.reduce_sum(Z**2, axis=1), [1,-1])
-    distance = X_norm + Z_norm - 2*tf.matmul(X,tf.transpose(Z))
-    return distance
+    
+    X_norm = tf.reshape(tf.reduce_sum(tf.square(X),axis=1), [-1,1])
+    Z_norm = tf.reshape(tf.reduce_sum(tf.square(Z), axis=1), [1,-1])
+    dist = X_norm + Z_norm - 2*tf.matmul(X,tf.transpose(Z))
+    return dist
 
 
-def responsibility(test_point, train_data, K):
-        
-    '''
-    Compute the responsibilities of training points to a new test point (assigns k nearest training points to
-    test point to value of 1/k, the rest to 0)
-         
-    Input: test_point 1 X D vector, training data is N X D vector, k is number of nearest neighbours
-    Output: responsbility vector N X D
-    
-    test_point_value (for example) = 3 by 1 dimension
-    
-    train_data.shape = 80 points by 1 dimension
-    
-    nearest_neighbour_indices sorted by closest training points to test point = 
-      [31, 60, 47, 66, 49, 48, 55, 27, 42, 76,  3, 52, 20, 40, 44, 78, 34,
-       18, 13,  4, 26, 33,  1, 29, 17, 25, 22, 67, 54, 62, 23,  5, 32, 51,
-       28, 39, 75, 43, 24, 61, 59, 63, 71,  2, 50, 69, 21,  7, 36, 45, 15,
-       19, 53, 56, 65, 64, 72, 41, 11, 38, 74, 70, 68, 46,  0, 79,  8, 16,
-       77, 30, 35, 10,  9, 58, 73, 57, 14,  6, 37, 12]
-        
-    r (for e.g. k = 3)[nearest_neighbour_idx = 31,60,47] = 
-      [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.33333333,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.33333333,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.33333333,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-        0.        ,  0.        ,  0.        ,  0.        ,  0.        ])
+def k_nearest_neighbours(X, X_test, k):
     
     '''
-    #Tensorflow implementation of responsibility function - this needs testing and debugging
-    r = tf.Variable(tf.zeros([train_data.shape[0],1])
-    distances = D_euc(test_point, train_data)
-    nearest_k_train_values, nearest_k_indices = tf.nn.top_k(distances, k=K, sorted = False)
-    r_k = r[nearest_k_indices].assign(1/K)
-    return r_k
+    Computes the k nearest neighbours based on the squared euclidean dist to the test point (X_test) to the training points (X)
+    '''
+    
+    neg_dist = tf.negative(dist_euclid(X_test, X))
+    k_nearest_values, k_nearest_indices = tf.nn.top_k(neg_dist, k=k, sorted=True, name='k_nearest')
+    return k_nearest_values, k_nearest_indices
 
+
+def responsibility_matrix(X, X_test, k):
+    
+    neg_dist = tf.negative(dist_euclid(X_test, X))
+    k_nearest_values, k_nearest_indices = tf.nn.top_k(neg_dist, k=k, sorted=True, name='k_nearest')
+    r_mat_shape = (X.shape[0], X_test.shape[0])
+    sparse_r_mat = tf.SparseTensor(indices=k_nearest_indices, values=1/k, dense_shape=r_mat_shape)
+    r_mat = tf.add(tf.zeros(shape=r_mat_shape), sparse_r_mat, name="r_mat")
+    return r_mat
+
+
+def knn_predict(X, y, X_pred, k):
+    
+    #r_mat = responsibility_matrix(X, X_pred, k)
+    _, k_nearest_indices = k_nearest_neighbours(X, X_pred, k)
+    k_nearest_y = tf.gather(y, indices=k_nearest_indices, name='k_nearest_y')
+    y_pred = tf.reduce_mean(k_nearest_y, axis=1)
+    #y_pred = tf.matmul(tf.transpose(r_mat), y)
+    return y_pred
+   
 if __name__ == '__main__':
     
-    #data generation given in the assignment
+    #Data generation given in the assignment
     np.random.seed(521)
     Data = np.linspace(1.0 , 10.0 , num =100) [:, np. newaxis]
     Target = np.sin( Data ) + 0.1 * np.power( Data , 2) + 0.5 * np.random.randn(100 , 1)
@@ -93,20 +79,47 @@ if __name__ == '__main__':
     validData, validTarget = Data[randIdx[80:90]], Target[randIdx[80:90]]
     testData, testTarget = Data[randIdx[90:100]], Target[randIdx[90:100]]
     
-    # Tensor Placeholders for train, validate and test data
-    train_data =    tf.placeholder(shape=[None, 1], dtype=tf.float32)
-    train_target =  tf.placeholder(shape=[None, 1], dtype=tf.float32)
-    valid_data =    tf.placeholder(shape=[None, 1], dtype=tf.float32)
-    valid_target =  tf.placeholder(shape=[None, 1], dtype=tf.float32)
-    test_data =     tf.placeholder(shape=[None, 1], dtype=tf.float32)
-    test_target =   tf.placeholder(shape=[None, 1], dtype=tf.float32)
+    data_set = (trainData, validData, testData)
+    target_set = (trainTarget, validTarget, testTarget)
+    set_names = ("train", "validation", "test")
+    k_list = (1, 3, 5, 50)
     
-    #Create the Tensorflow Graph
-    init_op = tf.global_variables_initializer()
-    with tf.Session() as sess:
-        sess.run(init_op)
-        # Code for predictions
+    #Create Graph and Placeholders
+    knn = tf.Graph()
     
- 
+    with knn.as_default():    
+        X =         tf.placeholder(dtype=tf.float32, name='X')
+        y =         tf.placeholder(dtype=tf.float32, name='y')
+        k =         tf.placeholder(dtype=tf.int32, name='k')
+        y_true =    tf.placeholder(dtype=tf.float32, name='y_true')
     
+        with tf.name_scope('predictions'):
+            X_pred = tf.placeholder(dtype=tf.float32, name='X_pred')
+            y_pred = knn_predict(X, y, X_pred, k)
     
+        with tf.name_scope('mse_loss'):
+            loss = tf.losses.mean_squared_error(labels=y_true, predictions=y_pred)
+    
+        init_op = tf.global_variables_initializer()
+        
+        with tf.Session() as sess:
+            sess.run(init_op)
+            
+            for K in k_list:
+                print("\nFor k = {}:".format(K))
+                for data, target, name in zip(data_set, target_set, set_names):
+                    print("For the {} set:".format(name))
+                    error = sess.run(loss, feed_dict={X: trainData, y: trainTarget, X_pred: data, y_true: target, k: K})
+                    print(error)
+            
+                X_Data = np.linspace(0.0, 11.0, num=1000)[:,np.newaxis]
+            
+                Pred = (sess.run(y_pred, feed_dict={X: trainData, y: trainTarget, X_pred: X_Data, k: K}))
+                
+                plt.title('KNN Regression Predictions')
+                plt.xlabel('X')
+                plt.ylabel('Y')
+                plt.grid(True)
+                plt.scatter(x=Data, y=Target)
+                plt.step(x=X_Data, y=Pred,c='r')
+                plt.show()
